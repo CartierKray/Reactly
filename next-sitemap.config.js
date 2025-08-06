2c832d838bb90985c13b60b234e1f91daaddaad2
Krishan Ramdin <Krishanramdin_@hotmail.com>
2025-08-06 13:57:37 +0200

    Send Changes Upstream

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.beterlease.nl', // Verander naar jouw eigen domein
+    siteUrl: 'https://www.reactly.nl', // Verander naar jouw eigen domein
     generateRobotsTxt: true, // Genereert automatisch een robots.txt
     sitemapSize: 5000, // Aantal links per sitemapbestand (standaard is 5000)
     exclude: ['/404', '/admin'], // Optioneel: pagina's om uit te sluiten
